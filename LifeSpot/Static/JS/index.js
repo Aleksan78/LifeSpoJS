5aab72e12e19a1f8c4670de5879fce03dc6d44f5
Alex <aleksandr.kopachev@mail.ru>
2025-11-06 14:43:58 +0300

    Добавьте файлы проекта.

---
+++ LifeSpot/Static/JS/index.js
@@ -0,0 +1,13 @@
+﻿let userName = prompt("Привет, как тебя зовут");
+alert(`Приветствуем, ${userName}. В твоём имени ${userName.length} символов`)
+
+let age = prompt("Введите ваш возраст");
+
+if (age >= 18) {
+    alert("Приветствуем на LifeSpot! " + new Date().toLocaleString());
+}
+else {
+    alert("Данный сайт имеет возрастные ограничения, до свидания");
+    //window.location.href = "http://www.google.com"
+    window.location.href = "https://youtu.be/1X2dUnEp5Ag?si=67FHpItpHS0JLM0a"/*Так прикольнее*/
+}
